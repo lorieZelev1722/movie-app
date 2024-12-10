87a45c5f39e9889ef62aabb8371493c9e2ffffb3
lorieZelev1722 <velezloremie046@gmail.com>
2024-12-10 14:28:23 +0800

    cc

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: ' https://loriezelev1722.github.io/',
+  base: ' /movie-app/',
   plugins: [react()],
 })
